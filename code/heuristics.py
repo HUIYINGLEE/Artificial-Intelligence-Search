b80150da49ac449d3ead26ac7d2aad92b787c4e9
HUIYINGLEE <HUIYINGLEE@users.noreply.github.com>
2017-09-05 17:18:18 +1000

    Update heuristics.py

--- code/heuristics.py
+++ code/heuristics.py
@@ -1,23 +1,9 @@
-# heuristics.py
-# ----------------
-# COMP3620/6320 Artificial Intelligence
-# The Australian National University
-# For full attributions, see attributions.txt on Wattle at the end of the course
 
 """ This class contains heuristics which are used for the search procedures that
     you write in search_strategies.py.
 
-    The first part of the file contains heuristics to be used with the algorithms
-    that you will write in search_strategies.py.
-
-    In the second part you will write a heuristic for Q4 to be used with a
-    MultiplePositionSearchProblem.
 """
 
-#-------------------------------------------------------------------------------
-# A set of heuristics which are used with a PositionSearchProblem
-# You do not need to modify any of these.
-#-------------------------------------------------------------------------------
 
 def null_heuristic(pos, problem):
     """ The null heuristic. It is fast but uninformative. It always returns 0.
@@ -42,12 +28,6 @@ null = null_heuristic
 manhattan = manhattan_heuristic
 euclidean = euclidean_heuristic
 
-#-------------------------------------------------------------------------------
-# You have to implement the following heuristics for Q4 of the assignment.
-# It is used with a MultiplePositionSearchProblem
-#-------------------------------------------------------------------------------
-
-#You can make helper functions here, if you need them
 
 def bird_counting_heuristic(state, problem) :
     position, yellow_birds = state
